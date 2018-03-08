22007b28db580504dcc39113a3eef1bb78057edf
Handrew <andrew.a.han@gmail.com>
2018-03-07 20:24:40 -0500

    finished with everything now just making more questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -37,26 +37,24 @@ $(document).ready(function () {
     //hiding the "time remaining" portion in the html
     $(".RT").hide();
     $("button").on("click", function () {
-        if(restarting === false) {
+        if (restarting === false) {
             restarting = true;
             restart();
         }
         $(".RT").show();
         start();
         $("#choices, #question").show();
-        
+
     })
-// start function that will start the countdown and then goes to the 'next' function
+    // start function that will start the countdown and then goes to the 'next' function
     function start() {
-        console.log(answerVar);
         $("button").hide()
         intervalId = setInterval(decrement, 1000);
         next();
     }
-//This function shows the question from the array.  The question goes in order
+    //This function shows the question from the array.  The question goes in order
     function next() {
 
-        console.log(nextQuestion);
         $("#question").html("<h3>" + questions[nextQuestion][0] + "</h3>");
         $("#a").html("<h4 value='a'>" + questions[nextQuestion][1] + "</h4>");
         $("#b").html("<h4 value='b'>" + questions[nextQuestion][2] + "</h4>");
@@ -64,25 +62,15 @@ $(document).ready(function () {
         $("#d").html("<h4 value='d'>" + questions[nextQuestion][4] + "</h4>");
     }
 
-// click function which adds the correct and incorrect answers
+    // click function which adds the correct and incorrect answers
     $("#a, #b, #c, #d").click(function () {
 
         var a = $(this).text();
         if (a === answerVar) {
             correct++;
             number = 15;
-
-            if (nextQuestion == questions.length - 1) {
-                $("#question, #choices").hide();
-                gameOver();
-            }
-
             stop();
-            nextQuestion++;
-            console.log(nextQuestion);
-            answerVar = questions[nextQuestion][5];
-            start();
-
+            greatJob();
         } else {
             incorrect++;
             stop();
@@ -90,7 +78,73 @@ $(document).ready(function () {
         }
 
     })
+    function greatJob() {
+        if (nextQuestion == questions.length - 1) {
+            imagesHere = questions[nextQuestion][6];
+            $("#choices").hide();
+            $("#question").html("<h1> Correct! The correct answer is: <br>" + answerVar + "</h1>");
+            $("#images").show().html(imagesHere);
 
+            setTimeout(function () {
+                $("#question, #choices, #images").hide();
+                gameOver();
+            }, 3000);
+        }
+        imagesHere = questions[nextQuestion][6];
+        $("#choices").hide();
+        $("#question").html("<h1> Great Job, NERD! The answer is: <br>" + answerVar + "</h1>");
+        $("#images").show().html(imagesHere);
+
+        nextQuestion++;
+        answerVar = questions[nextQuestion][5];
+        setTimeout(function () {
+            start();
+            $("#images").hide();
+            $("#choices").show();
+            $("#question").html("<h3>" + questions[nextQuestion][0] + "</h3>");
+            $("#a").html("<h4 value='a'>" + questions[nextQuestion][1] + "</h4>");
+            $("#b").html("<h4 value='b'>" + questions[nextQuestion][2] + "</h4>");
+            $("#c").html("<h4 value='c'>" + questions[nextQuestion][3] + "</h4>");
+            $("#d").html("<h4 value='d'>" + questions[nextQuestion][4] + "</h4>");
+        }, 3000);
+    }
+
+    // big function that gets called when the user picks the wrong answer
+    function sorry() {
+        //this will show if you got the last question wrong and then go over to the gameOver function
+        if (nextQuestion == questions.length - 1) {
+            imagesHere = questions[nextQuestion][6];
+            $("#choices").hide();
+            $("#question").html("<h1> Sorry! The correct answer was: <br>" + answerVar + "</h1>");
+            $("#images").show().html(imagesHere);
+
+            setTimeout(function () {
+                $("#question, #choices, #images").hide();
+                gameOver();
+            }, 3000);
+        }
+
+        imagesHere = questions[nextQuestion][6];
+        $("#choices").hide();
+        $("#question").html("<h1> Sorry~ You are not a NERD! The correct answer was: <br>" + answerVar + "</h1>");
+        $("#images").show().html(imagesHere);
+
+        nextQuestion++;
+        answerVar = questions[nextQuestion][5];
+
+        setTimeout(function () {
+            start();
+            $("#images").hide();
+            $("#choices").show();
+            $("#question").html("<h3>" + questions[nextQuestion][0] + "</h3>");
+            $("#a").html("<h4 value='a'>" + questions[nextQuestion][1] + "</h4>");
+            $("#b").html("<h4 value='b'>" + questions[nextQuestion][2] + "</h4>");
+            $("#c").html("<h4 value='c'>" + questions[nextQuestion][3] + "</h4>");
+            $("#d").html("<h4 value='d'>" + questions[nextQuestion][4] + "</h4>");
+        }, 3000);
+
+    }
+    //function to count down from the remaining time
     function decrement() {
         number--;
         $("#timer").text(number);
@@ -100,49 +154,10 @@ $(document).ready(function () {
             $("#timer").html("<h2>Times Run Out! Start Over!</h2>");
         }
     }
-
+    //function to stop the setInterval's timing
     function stop() {
         clearInterval(intervalId);
     }
-// big function that gets called when the user picks the wrong answer
-    function sorry() {
-        if (nextQuestion == questions.length - 1) {
-            imagesHere = questions[nextQuestion][6];
-            $("#choices").hide();
-            $("#question").html("<h1> Sorry! The correct answer was: <br>" + answerVar + "</h1>");
-
-            $("#images").show().html(imagesHere);
-            
-            setTimeout(function() {
-                $("#question, #choices, #images").hide();
-                gameOver();
-            }, 3000);      
-        }
-
-        imagesHere = questions[nextQuestion][6];
-        $("#choices").hide();
-        $("#question").html("<h1> Sorry! The correct answer was: <br>" + answerVar + "</h1>");
-        $("#images").show().html(imagesHere);
-
-        nextQuestion++;
-        console.log(nextQuestion);
-        answerVar = questions[nextQuestion][5];
-
-        setTimeout(function () {
-            start();
-            $("#images").hide();
-            $("#choices").show();
-            $("#question").html("<h3>" + questions[nextQuestion][0] + "</h3>");
-
-            $("#a").html("<h4 value='a'>" + questions[nextQuestion][1] + "</h4>");
-            $("#b").html("<h4 value='b'>" + questions[nextQuestion][2] + "</h4>");
-            $("#c").html("<h4 value='c'>" + questions[nextQuestion][3] + "</h4>");
-            $("#d").html("<h4 value='d'>" + questions[nextQuestion][4] + "</h4>");
-            console.log(answerVar);
-        }, 3000);
-
-    }
-
     // game over function that will tell you your scores and another button that will restart the game
     function gameOver() {
         $("#question, #choices").show();
@@ -157,13 +172,10 @@ $(document).ready(function () {
     // zero-ing on all the variables so that we can start the game over BUT not resetting.
     function restart() {
         $("#final").hide();
-        nextQuestion = 0, correct = 0, incorrect= 0, number = 15;
+        nextQuestion = 0, correct = 0, incorrect = 0, number = 15;
         answerVar = questions[nextQuestion][5];
-        console.log(nextQuestion);
-        console.log(restart);
 
         $("#question").html("<h3>" + questions[nextQuestion][0] + "</h3>");
-
         $("#a").html("<h4 value='a'>" + questions[nextQuestion][1] + "</h4>");
         $("#b").html("<h4 value='b'>" + questions[nextQuestion][2] + "</h4>");
         $("#c").html("<h4 value='c'>" + questions[nextQuestion][3] + "</h4>");
